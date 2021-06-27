8663b2160f09876dd1708bfe7b995cf39aee3cd2
Geetha <kakumanugeetha@gmail.com>
2021-06-27 18:28:33 +0530

    movie catalog

---
+++ MovieCatalog-dnc/src/main/java/dnc/sp/ms/mcatalog1/MovieCatalogDncApplication.java
@@ -0,0 +1,13 @@
+package dnc.sp.ms.mcatalog1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogDncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogDncApplication.class, args);
+	}
+
+}
---
+++ MovieCatalog-dnc/src/test/java/dnc/sp/ms/mcatalog1/MovieCatalogDncApplicationTests.java
@@ -0,0 +1,13 @@
+package dnc.sp.ms.mcatalog1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogDncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
