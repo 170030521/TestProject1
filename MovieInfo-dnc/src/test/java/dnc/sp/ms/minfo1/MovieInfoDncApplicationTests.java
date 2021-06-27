5f94364b10f96b9e59f4ee75247155fe6bf221a9
Phani0505 <170030521@kluniversity.in>
2021-06-27 18:30:12 +0530

    movie info

---
+++ MovieInfo-dnc/src/test/java/dnc/sp/ms/minfo1/MovieInfoDncApplicationTests.java
@@ -0,0 +1,13 @@
+package dnc.sp.ms.minfo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoDncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
